1a74c4c5163506d859e15bed891cf436b646d541
Maggie Walker <mags21walker@gmail.com>
2019-03-26 14:23:29 -0400

    Changed title and home page text placement

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-window.addEventListener('load', function() {
-	// store tabs variable
-    var myTabs = document.querySelectorAll('.navbar-nav > a');
-  function myTabClicks(tabClickEvent) {
+
+if (process.browser) {
+	window.addEventListener('load', function() {
+		// store tabs variable
+		var myTabs = document.querySelectorAll('.navbar-nav > a');
+	  function myTabClicks(tabClickEvent) {
+			for (let i = 0; i < myTabs.length; i++) {
+				myTabs[i].classList.remove('active');
+			}
+			var clickedTab = tabClickEvent.currentTarget;
+			clickedTab.classList.add('active');
+			tabClickEvent.preventDefault();
+			var myContentPanes = document.querySelectorAll('.tab-pane');
+			for (let i = 0; i < myContentPanes.length; i++) {
+				myContentPanes[i].classList.remove('active');
+			}
+			var anchorReference = tabClickEvent.target;
+			var activePaneId = anchorReference.getAttribute('href');
+			var activePane = document.querySelector(activePaneId);
+			activePane.classList.add('active');
+		}
 		for (let i = 0; i < myTabs.length; i++) {
-            myTabs[i].classList.remove('active');
+			myTabs[i].addEventListener('click', myTabClicks)
 		}
-        var clickedTab = tabClickEvent.currentTarget;
-		clickedTab.classList.add('active');
-		tabClickEvent.preventDefault();
-		var myContentPanes = document.querySelectorAll('.tab-pane');
-		for (let i = 0; i < myContentPanes.length; i++) {
-			myContentPanes[i].classList.remove('active');
-		}
-		var anchorReference = tabClickEvent.target;
-		var activePaneId = anchorReference.getAttribute('href');
-		var activePane = document.querySelector(activePaneId);
-        activePane.classList.add('active');
-	}
-	for (let i = 0; i < myTabs.length; i++) {
-		myTabs[i].addEventListener('click', myTabClicks)
-	}
-});
+	});
+}
